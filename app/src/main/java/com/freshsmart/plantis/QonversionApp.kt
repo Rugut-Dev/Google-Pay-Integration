82b09100c8477427a8f1dcfc6dec1f0623060d8e
Kelvin Rugut <krugut36@gmail.com>
2023-10-17 20:10:22 +0300

    Final Commit

---
+++ app/src/main/java/com/freshsmart/plantis/QonversionApp.kt
@@ -0,0 +1,15 @@
+package com.freshsmart.plantis
+
+import android.app.Application
+import com.qonversion.android.sdk.Qonversion
+
+class QonversionApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Qonversion.setDebugMode()
+        Qonversion.launch(this,
+            key = "P3_iwFqnTqsmY9OqIqjPj5LYhnchktvr",
+            false
+        )
+    }
+}
